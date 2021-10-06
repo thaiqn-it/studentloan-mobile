5ef4f3ce372a3b4c3fe8798bdc8783e7d376bcad
truongphi00 <kumayukito112@gmail.com>
2021-10-06 19:36:04 +0700

    update Home

--- pages/Home.js
+++ pages/Home.js
@@ -1,32 +1,75 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, SafeAreaView, FlatList } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  FlatList,
+} from "react-native";
 import ProgressCircle from "react-native-progress-circle";
 import { SearchBar, Avatar } from "react-native-elements";
 import { Icon } from "react-native-elements/dist/icons/Icon";
+import SectionedMultiSelect from "react-native-sectioned-multi-select";
+import {PRIMARY_COLOR, PRIMARY_COLOR_WHITE} from '../constants/styles'
 
 export default function Home({ navigation }) {
-  const [nameMajor, setNamemajor] = useState([
+  const [items, setItems] = useState([
+    // this is the parent or 'item'
     {
-      key: 1,
-      label: "Software Engineer",
+      name: "IT",
+      id: 1,
+      // these are the children or 'sub items'
+      children: [
+        {
+          name: "SE",
+          id: 1,
+        },
+        {
+          name: "GD",
+          id: 2,
+        },
+        {
+          name: "AI",
+          id: 3,
+        },
+        {
+          name: "IoT",
+          id: 4,
+        },
+        {
+          name: "SI",
+          id: 5,
+        },
+      ],
     },
     {
-      key: 2,
-      label: "Internet of Things",
-    },
-    {
-      key: 3,
-      label: "Internet of Things",
-    },
-    {
-      key: 4,
-      label: "Internet of Things",
-    },
-    {
-      key: 5,
-      label: "Internet of Things",
+      name: "Kinh doanh",
+      id: 2,
+      children: [
+        {
+          name: "asdasd",
+          id: 6,
+        },
+        {
+          name: "GadD",
+          id: 7,
+        },
+        {
+          name: "AeqweI",
+          id: 8,
+        },
+        {
+          name: "IoTdqwe",
+          id: 9,
+        },
+        {
+          name: "SI2312",
+          id: 10,
+        },
+      ],
     },
   ]);
+  const [selectedItems, setSelectedItems] = useState("");
 
   const [dataItems, setDataItems] = useState([
     {
@@ -51,6 +94,28 @@ export default function Home({ navigation }) {
       experiedDay: "03/10/2021",
       processStatus: 80,
     },
+    {
+      id: 3,
+      name: "Lương Thanh Hà",
+      school: "FPT University",
+      sesmester: "7",
+      major: "Software Engineering",
+      money: "25.000.000 VNĐ",
+      status: "active",
+      experiedDay: "03/10/2021",
+      processStatus: 55,
+    },
+    {
+      id: 4,
+      name: "Đinh Phú Cường",
+      school: "FPT University",
+      sesmester: "8",
+      major: "Software Engineering",
+      money: "25.000.000 VNĐ",
+      status: "active",
+      experiedDay: "03/10/2021",
+      processStatus: 95,
+    },
   ]);
   const [searchValue, setSearchValue] = useState("");
 
@@ -58,8 +123,9 @@ export default function Home({ navigation }) {
     return (
       <View
         style={{
+          padding:15,
           borderRadius: 20,
-          backgroundColor: "#ffffff",
+          backgroundColor: PRIMARY_COLOR_WHITE,
           marginTop: 20,
           marginStart: 10,
           marginEnd: 10,
@@ -69,9 +135,7 @@ export default function Home({ navigation }) {
         <Avatar
           rounded
           containerStyle={{
-            marginStart: 10,
-            marginBottom: "auto",
-            marginTop: "auto",
+            margin:10,
           }}
           size="large"
           source={{
@@ -88,37 +152,29 @@ export default function Home({ navigation }) {
         >
           <View>
             <Text
-              style={{
-                marginStart: 5,
-              }}
+              style={styles.textItem}
             >
-              - Name: {item.name}
+              Name: {item.name}
             </Text>
             <Text
-              style={{
-                marginStart: 5,
-              }}
+              style={styles.textItem}
             >
-              - School: {item.school}
+              School: {item.school}
             </Text>
             <Text
-              style={{
-                marginStart: 5,
-              }}
+              style={styles.textItem}
             >
-              - Ses: {item.sesmester}
+              Ses: {item.sesmester}
             </Text>
             <Text
-              style={{
-                marginStart: 5,
-              }}
+             style={styles.textItem}
             >
-              - Major: {item.major}
+              Major: {item.major}
             </Text>
 
             <View
               style={{
-                borderBottomColor: "#00b359",
+                borderBottomColor: PRIMARY_COLOR,
                 borderBottomWidth: 1,
                 marginTop: 10,
               }}
@@ -135,32 +191,39 @@ export default function Home({ navigation }) {
                 style={{
                   fontSize: 20,
                   marginTop: "auto",
+                  color:PRIMARY_COLOR,
                   marginBottom: "auto",
                 }}
               >
                 {item.money}
               </Text>
 
-              <View style={{
-                flexDirection:"row",
-              }}>
-                <Icon
-                containerStyle={{
-                  marginBottom:"auto",
-                  marginTop:"auto",
+              <View
+                style={{
+                  flexDirection: "row",
                 }}
+              >
+                <Icon
+                  containerStyle={{
+                    marginBottom: "auto",
+                    marginTop: "auto",
+                  }}
                   name="clock"
                   type="font-awesome-5"
-                  color="#00b359"
+                  color={PRIMARY_COLOR}
                   size={15}
                 />
 
                 <Text
-                style={{
-                  marginBottom:3,
-                  marginStart:5,
-                  fontSize:15,
-                }}>{item.experiedDay}</Text>
+                  style={{
+                    marginBottom: 3,
+                    marginStart: 5,
+                    fontSize: 15,
+                    color:PRIMARY_COLOR,
+                  }}
+                >
+                  {item.experiedDay}
+                </Text>
               </View>
             </View>
 
@@ -176,7 +239,7 @@ export default function Home({ navigation }) {
                 percent={item.processStatus}
                 radius={25}
                 borderWidth={5}
-                color="#00b359"
+                color={PRIMARY_COLOR}
                 shadowColor="#999"
                 bgColor="#fff"
               >
@@ -197,8 +260,8 @@ export default function Home({ navigation }) {
           <Icon
             name="angle-right"
             type="font-awesome-5"
-            color="#00b359"
-            size={68}
+            color={PRIMARY_COLOR}
+            size={20}
             onPress={() => navigation.navigate("DetailPost", item)}
           />
         </View>
@@ -207,30 +270,41 @@ export default function Home({ navigation }) {
   }
 
   return (
-    <SafeAreaView style={{ paddingTop: 50 }}>
-      <View
-        style={{
-          flexDirection: "row",
-        }}
-      >
-        <Text style={styles.header}>Explore</Text>
-      </View>
+    <SafeAreaView style={{ backgroundColor:PRIMARY_COLOR,flex: 1, paddingTop: 50 }}>
       <SearchBar
-        platform="android"
+        platform="ios"
         containerStyle={{
           marginTop: 10,
           marginEnd: 10,
           marginStart: 10,
-          borderRadius: 200,
+          borderRadius:10,
+          backgroundColor:PRIMARY_COLOR
+        }}
+        cancelButtonProps={{
+          color:PRIMARY_COLOR_WHITE
         }}
         placeholder="Type here to search"
         onChangeText={setSearchValue}
         value={searchValue}
       />
+      <SectionedMultiSelect
+        items={items}
+        IconRenderer={Icon}
+        colors={{
+          primary: "#00b359",
+        }}
+        uniqueKey="id"
+        subKey="children"
+        selectText="Press here to filter major!"
+        showDropDowns={true}
+        readOnlyHeadings={true}
+        onSelectedItemsChange={setSelectedItems}
+        selectedItems={selectedItems}
+      />
       <FlatList
         data={dataItems}
         renderItem={_renderItem}
-        keyExtractor={(item) => item.id}
+        idExtractor={(item) => item.id}
       />
     </SafeAreaView>
   );
@@ -246,4 +320,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#FFFF",
     borderRadius: 10,
   },
+  textItem:{
+    color: PRIMARY_COLOR,
+    marginStart: 5,
+    fontSize:15,
+  }
 });
